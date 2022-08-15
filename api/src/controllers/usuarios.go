4a3848a1d524a2a6d2df6866f96c333db7e9f46c
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-08-15 15:29:04 -0300

    add metodos para as rotas de usuarios

---
+++ api/src/controllers/usuarios.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// CriarUsuario insere um usuário no banco de dados
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// BuscarUsuarios busca todos os usuarios salvos no banco de dados
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+// BuscarUsuario busca um usuário salvo no banco de dados
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+// Altera as informações do usuario salvo no banco de dados
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+// DeletarUsuario deleta um usuário salvo no banco de dados
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+	
+}
