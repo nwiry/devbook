3beabaab93890bddc022645c6a718c7911e04566
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-08-15 22:33:46 -0300

    model usuario add

---
+++ api/src/models/Usuario.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+// Usuario representa os dados do usuário utilizando a aplicação
+type Usuario struct {
+	ID   int    `json:"id,omitempty"`
+	Nome string `json:"nome,omitempty"`
+	Nick string `json:"nick,omitempty"`
+	Email string `json:"email,omitempty"`
+	Senha string `json:"senha,omitempty"`
+	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
