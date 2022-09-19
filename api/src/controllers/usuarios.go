79c94c3ae076e395596a08d5f9af6d5e9bf2d12c
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-09-19 20:06:33 -0300

    added input validations

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -24,6 +24,11 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if erro = usuario.Preparar(); erro != nil {
+		responses.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
 	db, erro := database.Conectar()
 	if erro != nil {
 		responses.Erro(w, http.StatusInternalServerError, erro)
--- api/src/models/Usuario.go
+++ api/src/models/Usuario.go
@@ -1,13 +1,51 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Usuario representa os dados do usuário utilizando a aplicação
 type Usuario struct {
-	ID   int    `json:"id,omitempty"`
-	Nome string `json:"nome,omitempty"`
-	Nick string `json:"nick,omitempty"`
-	Email string `json:"email,omitempty"`
-	Senha string `json:"senha,omitempty"`
+	ID       int       `json:"id,omitempty"`
+	Nome     string    `json:"nome,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
+
+func (usuario *Usuario) Preparar() error {
+
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
+
+	if usuario.Nome == "" {
+		return errors.New("o nome é obrigatório e não pode ficar em branco")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("o nick é obrigatório e não pode ficar em branco")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("o e-mail é obrigatório e não pode ficar em branco")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("a senha é obrigatória e não pode ficar em branco")
+	}
+
+	return nil
 }
