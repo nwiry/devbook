bfbcf94ad1db18b1e3d2f5e79a73735510633b54
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-09-19 19:58:02 -0300

    fixed errors responses

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -1,57 +1,62 @@
 package controllers
 
 import (
+	"api/responses"
 	"api/src/database"
 	"api/src/models"
 	"api/src/repositories"
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
+	"io"
 	"net/http"
 )
 
 // CriarUsuario insere um usuário no banco de dados
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	corpoRequest, erro := ioutil.ReadAll(r.Body)
+	corpoRequest, erro := io.ReadAll(r.Body)
 	if erro != nil {
-		log.Fatal(erro)
+		responses.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
 	}
 
 	var usuario models.Usuario
 	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
-		log.Fatal(erro)
+		responses.Erro(w, http.StatusBadRequest, erro)
+		return
 	}
 
 	db, erro := database.Conectar()
 	if erro != nil {
-		log.Fatal(erro)
+		responses.Erro(w, http.StatusInternalServerError, erro)
+		return
 	}
-	
+
 	repositorio := repositories.NovoRepositorioDeUsuarios(db)
 	enteredUser, err := repositorio.Criar(usuario)
 	if err != nil {
-		log.Fatal(err)
+		responses.Erro(w, http.StatusInternalServerError, err)
+		return
 	}
-	w.Write([]byte(fmt.Sprintf("ID Usuário Inserido: %d", enteredUser)))
+	usuario.ID = int(enteredUser)
+
+	responses.JSON(w, http.StatusCreated, usuario)
 }
 
 // BuscarUsuarios busca todos os usuarios salvos no banco de dados
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
-	
+
 }
 
 // BuscarUsuario busca um usuário salvo no banco de dados
 func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
-	
+
 }
 
 // Altera as informações do usuario salvo no banco de dados
 func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
-	
+
 }
 
 // DeletarUsuario deleta um usuário salvo no banco de dados
 func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
-	
+
 }
